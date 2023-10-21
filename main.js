0639e6b6661819dc067d9b5f57b3d7e89cb58a20
BigRedError <Nope>
2023-10-21 15:36:18 -0700

    ok

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 import * as THREE from 'https://unpkg.com/three@0.157.0/build/three.module.js';
 
-const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 3;
-const renderer = new THREE.WebGLRenderer(canvas: canvas);
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
